6b30219645e5b02938f94e5a8563175cc2b8b14d
mxt629 <xuantien.06021990@gmail.com>
2022-03-28 23:53:09 +0700

    Initial Commit

---
+++ SS15 Example4/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SS15 Example4
+//
+//  Created by Little Chick on 28/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
